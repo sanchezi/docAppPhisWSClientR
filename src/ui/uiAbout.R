9b8987ae655d8bb55a0964d293b1f98157f56410
sanchezi <isabelle.sanchez@inra.fr>
2018-05-31 15:49:16 +0200

    about modified

--- src/ui/uiAbout.R
+++ src/ui/uiAbout.R
@@ -21,7 +21,18 @@ tabItem(tabName="about",
                   p("Keep the default",tags$b("username and password")," if you want a public access to the WS"),
                   p("Please, give an",tags$b(" experiment URI")," for which you want to know the available variables"),
                   p("Click on the", HTML("<span class=\"fLetter\">submit button</span>"),", each time you change your settings"),
-                  p("You can retrieve the variable name usable in the",HTML("<span class=\"fLetter\">phisWSClientR get()</span>")," functions in the observationVariableId colname of each dataset")
+                  p("You can retrieve the variable names usable in the",
+                    HTML("<span class=\"fLetter\">phisWSClientR get()</span>"),
+                    " functions in the ",tags$b("observationVariableId"),
+                    " colname of each dataset")
+              )
+          ),
+          box(width=12, height = 200,
+              div(id="about2", class="about",
+                  style="margin: 0px 0px 50px 0px;",
+                  h1("Credits:"),
+                  p("isabelle.sanchez@inra.fr - INRA MISTEA - openSILEX - 2018"),
+                  p("GPL-3 license")
               )
           ),
           box(width=12, height = 500,
