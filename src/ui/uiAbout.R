f69c1042105411f8ee6378635073539acb05efd5
sanchezi <isabelle.sanchez@inra.fr>
2018-05-31 15:42:17 +0200

    new about file

--- src/ui/uiAbout.R
+++ src/ui/uiAbout.R
@@ -9,18 +9,27 @@
 
 tabItem(tabName="about",
         fluidRow(
-          box(width=12, height = 800,
-              div(id="about1", class="about",
-                  style="margin: 0px 0px 50px 0px;",
-                  h1("About panel"),
-                  verbatimTextOutput("about")
-                  )
-          ),
           box(width=12, height = 420,
               div(id="about2", class="about",
                   style="margin: 0px 0px 50px 0px;",
-                  p("du texte explicatif!!!!")
+                  h1("How to use this documentation App?"),
+                  p(HTML("<span>Please click on </span>"),
+                    HTML("<span class=\"fLetter\">WS access</span>"),
+                    HTML("<span> to scroll down the menu</span>")
+                  ),
+                  p("Enter a ",tags$b("username, password, server and port address")," if you want a private access to the phenomeapi WS"),
+                  p("Keep the default",tags$b("username and password")," if you want a public access to the WS"),
+                  p("Please, give an",tags$b(" experiment URI")," for which you want to know the available variables"),
+                  p("Click on the", HTML("<span class=\"fLetter\">submit button</span>"),", each time you change your settings"),
+                  p("You can retrieve the variable name usable in the",HTML("<span class=\"fLetter\">phisWSClientR get()</span>")," functions in the observationVariableId colname of each dataset")
               )
+          ),
+          box(width=12, height = 500,
+              div(id="about1", class="about",
+                  style="margin: 0px 0px 50px 0px;",
+                  h1("Session Info"),
+                  verbatimTextOutput("about")
+                  )
           )# end box
         )# end fluidRow
 )# end tabItem
